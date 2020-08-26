2365c352a0d61da06e91d1e8f0b2bf468c59791a
maxcroy1 <max.croy1@gmail.com>
2020-08-26 18:14:20 -0400

    Added first route and Home component

--- auto-grocer-frontend/src/App.js
+++ auto-grocer-frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ auto-grocer-frontend/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <div>
+                <h1>AutoGrocer</h1>
+                <p>Make ordering groceries from home easier than ever.</p>
+            </div>
+            <div>
+                <h2>About</h2>
+                <p>Enter copy here.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;

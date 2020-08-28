e551ca5ae6687985e394669d08ba2dfdecabad9c
maxcroy1 <max.croy1@gmail.com>
2020-08-28 18:20:39 -0400

    Started work on shopping flow

--- auto-grocer-frontend/src/containers/Shop.js
+++ auto-grocer-frontend/src/containers/Shop.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import Navbar from '../components/Navbar'
 
-function Shop() {
-    return (
-        <div>
-            <Navbar />
-            <h1>Let's shop!</h1>
-        </div>
-    );
+class Shop extends React.Component {
+    state = {
+        item: "",
+        quantity: "1"
+    }
+
+    quantityGenerator() {
+        let nums = []
+        for (let i = 1; i <= 9; i++) {
+            nums.push(i)
+        }
+        return nums
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <h1>Let's shop!</h1>
+                <h3>Enter the name and quantity of the item that you'd like to add to your order below</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="item">Item:</label><br />
+                    <input type="text" name="item" onChange={this.handleChange} />
+                    <select name="quantity" onChange={this.handleChange}>
+                        {this.quantityGenerator().map(num => <option value={num} selected={num === 1 ? true : false}>{num}</option>)}
+                    </select>
+                    <input type="submit" value="Add Item" />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Shop;

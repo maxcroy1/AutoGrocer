0c13f98bf357ec7578c54a8c0d93cebc1b40c0b0
maxcroy1 <max.croy1@gmail.com>
2020-08-30 10:55:49 -0400

    Updated Shop container to include nested routing

---
+++ auto-grocer-frontend/src/components/ItemSelection.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class ItemSelection extends React.Component {
+
+    state = {
+        item: "",
+        quantity: "1",
+        items: []
+    }
+
+    quantityGenerator() {
+        let nums = []
+        for (let i = 1; i <= 9; i++) {
+            nums.push(i)
+        }
+        return nums
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleAdd = (event) => {
+        event.preventDefault();
+        event.target.reset();
+        let newItem = {
+            item: this.state.item,
+            quantity: this.state.quantity
+        };
+        this.setState( previousState => ({
+            items: [...previousState.items, newItem]
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Enter the name and quantity of the item that you'd like to add to your order below</h3>
+                <form onSubmit={this.handleAdd}>
+                    <label htmlFor="item">Item:</label><br />
+                    <input type="text" name="item" onChange={this.handleChange} />
+                    <select name="quantity" onChange={this.handleChange}>
+                        {this.quantityGenerator().map(num => <option value={num} selected={num === 1 ? true : false}>{num}</option>)}
+                    </select>
+                    <input type="submit" value="Add Item" />
+                </form>
+                <h3>Cart Items:</h3>
+                <ul>
+                    {this.state.items.map(item => <li><strong>{item.item}</strong> {item.quantity}x <button>Remove</button></li>)}
+                </ul>
+            </div>
+        );
+    }
+}
--- auto-grocer-frontend/src/containers/Shop.js
+++ auto-grocer-frontend/src/containers/Shop.js
@@ -1,60 +1,26 @@
 import React from 'react';
+import { Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
 import Navbar from '../components/Navbar'
+import ItemSelection from '../components/ItemSelection'
 
-class Shop extends React.Component {
-    state = {
-        item: "",
-        quantity: "1",
-        items: []
-    }
+const Shop = (props) => {
 
-    quantityGenerator() {
-        let nums = []
-        for (let i = 1; i <= 9; i++) {
-            nums.push(i)
-        }
-        return nums
-    }
+    let { path } = useRouteMatch();
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleAdd = (event) => {
-        event.preventDefault();
-        event.target.reset();
-        let newItem = {
-            item: this.state.item,
-            quantity: this.state.quantity
-        };
-        this.setState( previousState => ({
-            items: [...previousState.items, newItem]
-        }));
-    }
-
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <h1>Let's shop!</h1>
-                <h3>Enter the name and quantity of the item that you'd like to add to your order below</h3>
-                <form onSubmit={this.handleAdd}>
-                    <label htmlFor="item">Item:</label><br />
-                    <input type="text" name="item" onChange={this.handleChange} />
-                    <select name="quantity" onChange={this.handleChange}>
-                        {this.quantityGenerator().map(num => <option value={num} selected={num === 1 ? true : false}>{num}</option>)}
-                    </select>
-                    <input type="submit" value="Add Item" />
-                </form>
-                <h3>Cart Items:</h3>
-                <ul>
-                    {this.state.items.map(item => <li><strong>{item.item}</strong> {item.quantity}x <button>Remove</button></li>)}
-                </ul>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Navbar />
+            <h1>Let's shop!</h1>
+            <Switch>
+                <Route exact path={`${path}/item_selection`}>
+                    <ItemSelection />
+                </Route>
+                <Route exact path={`${path}`}>
+                    <Redirect to={`${path}/item_selection`} />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default Shop;
